b0bd1c1020eba7677489825d8afc0de3404f6bc3
CJ Wilson & tmpbtz <pair+cjwilsonjr.tmpbtz@devbootcamp.com>
2015-04-27 09:49:41 -0500

    wired up views for sessions, users, and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  belongs_to :author, class_name: "User"
+
+  validates :title, :body, presence: true
+
+  def time_since_creation
+    ((Time.now - created_at) / 3600).round
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_many :posts, foreign_key: :author_id
 
-  validates :first_name,
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :username,   presence: true, uniqueness: true
+  validates :email,      presence: true, uniqueness: true
+  validates :password,   presence: true, length: { minimum: 6 }
 
   has_secure_password
 end
