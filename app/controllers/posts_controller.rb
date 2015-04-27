a631f44042d707b3d62ad4cda8a667c762fb42ee
CJ Wilson & tmpbtz <pair+cjwilsonjr.tmpbtz@devbootcamp.com>
2015-04-27 10:54:03 -0500

    add posts controller-- index and new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,39 @@
 class PostsController < ApplicationController
+  before_action :set_scope, only: [:index]
+  # before_action :set_scope, only: [:index, :edit, :show]
+
+
+  def index
+    @posts = @scope.all
+    render "index"
+  end
+
+  def new
+    if session[:user_id]
+      @post = Post.new
+      render "new"
+    end
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  protected
+
+  def set_scope
+    @scope ||= params[:user_id] ? Post.where(author: params[:user_id]) : Post
+  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render "new"
+  end
+
+  def create
+    user = User.find_by_username(params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_posts_path(user)
+    else
+      @errors = ["You have entered an invalid username/password"]
+      render "new"
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to "root"
+  end
 end
